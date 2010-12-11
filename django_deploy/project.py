ec4b21cd3dba05e6648f0ea9a5058a94b5cc4f21
John Debs <johnthedebs@gmail.com>
2010-12-11 03:55:22 -0500

    Used sudo where appropriate in project.py, and finally got the sed command that turns off debugging to work

--- django_deploy/project.py
+++ django_deploy/project.py
@@ -9,7 +9,7 @@ from fabric.operations import put
 def build_static_media():
     "Build the static media for the project."
     # Purge the old before building the new
-    run("rm -rf /srv/www/%(project_name)s/site_media/static/*" % env)
+    sudo("rm -rf /srv/www/%(project_name)s/site_media/static/*" % env)
     manage("build_static --noinput")
 
 def createsuperuser():
@@ -26,23 +26,23 @@ def deploy_project():
 
 def disable_project():
     "Disable the site in Apache."
-    run("a2dissite %(project_name)s" % env)
+    sudo("a2dissite %(project_name)s" % env)
     reload_webserver()
 
 def enable_project():
     "Enable the site in Apache."
-    run("a2dissite %(project_name)s" % env)
+    sudo("a2dissite %(project_name)s" % env)
     reload_webserver()
 
 def install_project_requirements():
     "Install everything in requirements.txt to the project's virtualenv."
     with cd("/srv/www/%(project_name)s/%(project_name)s" % env):
-        run("workon %(project_name)s && pip install -U -r requirements.txt" % env)
+        sudo("workon %(project_name)s && pip install -U -r requirements.txt" % env)
 
 def redeploy_project():
     "Delete project code directory and redeploy. Data and logs remain intact."
     disable_project()
-    run("rm -rf /srv/www/%(project_name)s/%(project_name)s/*" % env)
+    sudo("rm -rf /srv/www/%(project_name)s/%(project_name)s/*" % env)
     deploy_project()
 
 def remove_project():
@@ -56,20 +56,22 @@ def remove_project():
 def setup_project():
     "Deploy a project for the first time."
     # TODO: Check that it is safe to run the setup
-    run("mkvirtualenv --no-site-packages %(project_name)s" % env)
+    sudo("mkvirtualenv --no-site-packages %(project_name)s" % env)
     # Create project's directory structure
-    run("mkdir -p /srv/www/%(project_name)s/%(project_name)s" % env)
+    sudo("mkdir -p /srv/www/%(project_name)s/%(project_name)s" % env)
     with cd("/srv/www/%(project_name)s" % env):
-        run("mkdir -p conf db logs site_media/static site_media/media")
-    # Set Apache permissions
-    run("chown www-data /srv/www/%(project_name)s/db" % env)
-    run("chown -R www-data /srv/www/%(project_name)s/site_media" % env)
+        sudo("mkdir -p conf db logs site_media/static site_media/media")
+    # Set permissions
+    sudo("chown -R %(project_name)s:%(project_name)s /srv/virtualenvs/%(project_name)s" % env)
+    sudo("chown -R www-data /srv/www/%(project_name)s/db" % env)
+    sudo("chown -R www-data /srv/www/%(project_name)s/site_media" % env)
+    # TODO: Any other permissions to set?
     upload_apache_config()
 
 def upload_apache_config():
     "Upload a customized apache configuration to the server."
-    env.project_site_packages = run('workon %(project_name)s && python -c "from distutils.sysconfig import get_python_lib; print get_python_lib();"' % env)
-    upload_template(template("apache.conf"), "/etc/apache2/sites-available/%(project_name)s" % env, context=env)
+    env.project_site_packages = sudo('workon %(project_name)s && python -c "from distutils.sysconfig import get_python_lib; print get_python_lib();"' % env)
+    upload_template(template("apache.conf"), "/etc/apache2/sites-available/%(project_name)s" % env, context=env, use_sudo=True)
 
 def upload_project():
     "Uploads the entire project to the server."
@@ -77,11 +79,12 @@ def upload_project():
     run("mkdir -p /srv/www/%(project_name)s/%(project_name)s" % env)
     # Package the appropriate branch, move it to the server, then clean up
     local("git archive --format=tar %(project_branch)s | gzip > project.tar.gz" % env)
-    put("project.tar.gz", "/srv/www/%(project_name)s/%(project_name)s/project.tar.gz" % env)
+    put("project.tar.gz", "~" % env)
+    sudo("mv project.tar.gz /srv/www/%(project_name)s/%(project_name)s/" % env)
     local("rm project.tar.gz")
     # Unpackage the code, then clean up
     with cd("/srv/www/%(project_name)s/%(project_name)s" % env):
-        run("tar zxf project.tar.gz")
-        run("rm project.tar.gz")
+        sudo("tar zxf project.tar.gz")
+        sudo("rm project.tar.gz")
         # Turn debugging off
-        #sed("setting.py", "DEBUG\w*=\w", "")
+        sudo("sed -i.bak -r -e 's/^[ \\t]*DEBUG[ \\t]*=[ \\t]*True[ \\t]*$/DEBUG = False/g' settings.py")
