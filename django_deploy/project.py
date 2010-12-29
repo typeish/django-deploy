75f40674175fb183ecf34389482c925682abea16
John Debs <johnthedebs@gmail.com>
2010-12-29 12:29:51 -0500

    Big rewrite of deploy_project: setup_project and redeploy_project are no longer needed and deploy_project makes timestamped releases in prep for rollbacks (coming soon). Also fixed a bunch of bugs and improved overall robustness.

--- django_deploy/project.py
+++ django_deploy/project.py
@@ -17,8 +17,26 @@ def createsuperuser():
     manage("createsuperuser %s" % username)
 
 def deploy_project():
-    "Deploy the project. Requires that the project has been set up first."
+    "Deploy the project."
+    if not exists("/srv/www/%(project_name)s" % env):
+        # Create project's directory structure
+        sudo("mkdir -p /srv/www/%(project_name)s" % env)
+        with cd("/srv/www/%(project_name)s" % env):
+            sudo("mkdir -p conf db logs releases site_media/static site_media/media")
+        # Set permissions
+        # TODO: Need to make sure this user exists before assigning them permissions
+        # This is so that a user can install things to their virtualenv without sudo
+        # It isn't needed yet, so leaving it out for now.
+        #sudo("chown -R %(project_name)s:%(project_name)s /srv/virtualenvs/%(project_name)s" % env)
+        sudo("chown -R www-data /srv/www/%(project_name)s/db" % env)
+        sudo("chown -R www-data /srv/www/%(project_name)s/site_media" % env)
+    if not exists("/srv/virtualenvs/%(project_name)s" % env):
+        sudo("mkvirtualenv %(project_name)s" % env)
+    if not exists("/etc/apache2/sites-available/%(project_name)s" % env):
+        upload_apache_config()
+    disable_project()
     upload_project()
+    link_release()
     install_project_requirements()
     build_static_media()
     manage("syncdb --noinput")
@@ -37,54 +55,50 @@ def enable_project():
 
 def install_project_requirements():
     "Install everything in requirements.txt to the project's virtualenv."
+    # Make sure we don't have existing source checkouts in the virtualenv
+    # They tend to cause upgrades to fail, which is annoying
+    if exists("/srv/virtualenvs/%(project_name)s/src" % env):
+        sudo("rm -rf /srv/virtualenvs/%(project_name)s/src" % env)
     with cd("/srv/www/%(project_name)s/%(project_name)s" % env):
-        sudo("workon %(project_name)s && pip install -I -r requirements.txt" % env)
+        sudo("workon %(project_name)s && pip install -U -r requirements.txt" % env)
 
-def redeploy_project():
-    "Delete project code directory and redeploy. Data and logs remain intact."
-    disable_project()
-    sudo("rm -rf /srv/www/%(project_name)s/%(project_name)s/*" % env)
-    deploy_project()
+def link_release():
+    "Remove previous symbolic link and create one to the new release"
+    if exists("/srv/www/%(project_name)s/%(project_name)s" % env):
+        sudo("rm /srv/www/%(project_name)s/%(project_name)s" % env)
+    with cd("/srv/www/%(project_name)s/" % env):
+        sudo("ln -s releases/%(deploy_time)s/ %(project_name)s" % env)
 
 def remove_project():
     "Totally remove a deployed project from the server."
     disable_project()
     # Remove Apache config, project directory, and virtualenv
-    run("rm /etc/apache2/sites-available/%(project_name)s" % env)
-    run("rm -rf /srv/www/%(project_name)s" % env)
-    run("rmvirtualenv %(project_name)s" % env)
-
-def setup_project():
-    "Deploy a project for the first time."
-    # TODO: Check that it is safe to run the setup
-    sudo("mkvirtualenv --no-site-packages %(project_name)s" % env)
-    # Create project's directory structure
-    sudo("mkdir -p /srv/www/%(project_name)s/%(project_name)s" % env)
-    with cd("/srv/www/%(project_name)s" % env):
-        sudo("mkdir -p conf db logs site_media/static site_media/media")
-    # Set permissions
-    sudo("chown -R %(project_name)s:%(project_name)s /srv/virtualenvs/%(project_name)s" % env)
-    sudo("chown -R www-data /srv/www/%(project_name)s/db" % env)
-    sudo("chown -R www-data /srv/www/%(project_name)s/site_media" % env)
-    # TODO: Any other permissions to set?
-    upload_apache_config()
+    # TODO: These should have proper permissions and NOT need to run as sudo
+    if exists("/etc/apache2/sites-available/%(project_name)s" % env):
+        sudo("rm /etc/apache2/sites-available/%(project_name)s" % env)
+    if exists("/srv/www/%(project_name)s" % env):
+        sudo("rm -rf /srv/www/%(project_name)s" % env)
+    if exists("/srv/virtualenvs/%(project_name)s" % env):
+        sudo("rmvirtualenv %(project_name)s" % env)
 
 def upload_apache_config():
     "Upload a customized apache configuration to the server."
+    # This is used to render the apache config template
     env.project_site_packages = sudo('workon %(project_name)s && python -c "from distutils.sysconfig import get_python_lib; print get_python_lib();"' % env)
     upload_template(template("apache.conf"), "/etc/apache2/sites-available/%(project_name)s" % env, context=env, use_sudo=True)
 
 def upload_project():
     "Uploads the entire project to the server."
-    # Make sure the project directory exists
-    run("mkdir -p /srv/www/%(project_name)s/%(project_name)s" % env)
+    # Create the release directory
+    run("mkdir -p /srv/www/%(project_name)s/releases/%(deploy_time)s" % env)
     # Package the appropriate branch, move it to the server, then clean up
     local("git archive --format=tar %(project_branch)s | gzip > project.tar.gz" % env)
+    # Upload to the home directory first, since we can't `sudo put`
     put("project.tar.gz", "~" % env)
-    sudo("mv project.tar.gz /srv/www/%(project_name)s/%(project_name)s/" % env)
+    sudo("mv project.tar.gz /srv/www/%(project_name)s/releases/%(deploy_time)s/" % env)
     local("rm project.tar.gz")
     # Unpackage the code, then clean up
-    with cd("/srv/www/%(project_name)s/%(project_name)s" % env):
+    with cd("/srv/www/%(project_name)s/releases/%(deploy_time)s" % env):
         sudo("tar zxf project.tar.gz")
         sudo("rm project.tar.gz")
         # Turn debugging off
