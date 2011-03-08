e7297fcf4b5da764ef78f60e85dae9368e729ac3
John Debs <johnthedebs@gmail.com>
2011-03-08 18:59:02 -0500

    Update Fabric requirement (to 1.0) and version number (to 0.2.0)

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements():
 
 setup(
     name="django-deploy",
-    version="0.1.0",
+    version="0.2.0",
     description="Django deployment utility",
     author="John Debs",
     author_email="johnthedebs@gmail.com",
