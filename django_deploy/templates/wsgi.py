b6808a405b8b991a4a5444ec8627c37b7d3038ab
John Debs <johnthedebs@gmail.com>
2011-03-08 12:44:51 -0500

    Tweaked template files, updated license, cleaned up setup.py

--- django_deploy/templates/wsgi.py
+++ django_deploy/templates/wsgi.py
@@ -6,12 +6,15 @@ import sys
 from os.path import abspath, dirname, join
 from site import addsitedir
 
-sys.path.insert(0, abspath(join(dirname(__file__), "../../")))
+# Avoid having stray print statements cause 500 errors
+sys.stdout = sys.stderr
+
+sys.path.insert(0, abspath(join(dirname(__file__), "..", "..")))
+sys.path.insert(0, abspath(join(dirname(__file__), "..")))
+sys.path.insert(0, abspath(join(dirname(__file__), "..", "apps")))
 
 from django.conf import settings
 os.environ["DJANGO_SETTINGS_MODULE"] = "%(project_name)s.settings"
-
-sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
 
 from django.core.handlers.wsgi import WSGIHandler
 application = WSGIHandler()
--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@
 from setuptools import setup, find_packages
 import os
 
-
 rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
 
 def read_from(filename):
@@ -15,25 +14,25 @@ def read_from(filename):
         fp.close()
 
 def get_requirements():
-    data = read_from(rel_file('requirements.txt'))
+    data = read_from(rel_file("requirements.txt"))
     lines = map(lambda s: s.strip(), data.splitlines())
     return filter(None, lines)
 
 setup(
-    name='django-deploy',
-    version='0.1.0',
-    description='Django deployment utility',
-    author='John Debs',
-    author_email='johnthedebs@gmail.com',
-    url='http://github.com/johnthedebs/django-deploy',
+    name="django-deploy",
+    version="0.1.0",
+    description="Django deployment utility",
+    author="John Debs",
+    author_email="johnthedebs@gmail.com",
+    url="http://github.com/johnthedebs/django-deploy",
     packages=find_packages(),
     install_requires=get_requirements(),
     classifiers=(
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
     ),
 )
